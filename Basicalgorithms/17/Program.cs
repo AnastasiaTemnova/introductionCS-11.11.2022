a003de2b8632beed09d0a1229b849ab725b63b5d
Темнова <nastena_petlilev@mail.ru>
2022-11-29 20:11:14 +0400

    lesson 2

---
+++ Basicalgorithms/17/Program.cs
@@ -0,0 +1,7 @@
+﻿
+// 17. Дано число обозначающее день недели. Выяснить является номер дня недели выходным
+
+System.Console.WriteLine ("Введите число дня недели от 1 до 7");
+int a= Convert.ToInt32(Console.ReadLine());
+if (a==6 || a==7) System.Console.WriteLine("Выходные");
+else System.Console.WriteLine("Рабочие"); 
