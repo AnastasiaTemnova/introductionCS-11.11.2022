8040e21644dca7fbaff9f9ef6dbccee89a70f9f5
Темнова <nastena_petlilev@mail.ru>
2022-12-08 19:46:23 +0400

    lesson 5

---
+++ 56/Program.cs
@@ -0,0 +1,17 @@
+﻿//56.Написать программу копирования массива
+
+int[] array = new int[5];
+int arrayCopy;
+
+Console.WriteLine();
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(0, 100);
+    Console.Write(array[i] + " ");
+}
+Console.WriteLine();
+for (int i = 0; i < array.Length; i++)
+{
+    arrayCopy = array[i];
+    Console.Write(arrayCopy + " ");
+}
