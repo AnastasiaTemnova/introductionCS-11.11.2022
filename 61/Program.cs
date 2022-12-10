2fe0a592a5e3a0855475ff17067c566377e8c082
Темнова <nastena_petlilev@mail.ru>
2022-12-10 22:57:12 +0400

    lesson 6

---
+++ 61/Program.cs
@@ -0,0 +1,23 @@
+﻿// 61.Показать двумерный массив размером m×n заполненный вещественными случайными числами
+double[,] Random2DArray(int m, int n)
+{
+    double[,] a = new double[m, n];
+    Random random = new Random();
+    for (int i = 0; i < m; i++)
+        for (int j = 0; j < n; j++)
+            a[i, j] = random.NextDouble();
+    return a;
+}
+void Print2DArray(double[,] a)
+{
+    for (int i = 0; i < a.GetLength(0); i++)
+    {
+        for (int j = 0; j < a.GetLength(1); j++)
+            System.Console.Write($"{a[i, j],7:F2}");
+        System.Console.WriteLine();
+    }
+}
+int m=3;
+int n=3;
+double[,] a = Random2DArray(m, n);
+Print2DArray(a);
---
+++ 62/Program.cs
@@ -0,0 +1,56 @@
+﻿//62. В двумерном массиве заменить элементы, у которых оба индекса чётные на их квадраты по значению.
+void PrintArray(double[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+            Console.Write($" {matr[i, j]} ");
+        Console.WriteLine();
+    }
+}
+void FindEvenIndex(double[,] matr)
+{
+
+    for (int i = 0; i < matr.GetLength(0); i++)
+
+
+        for (int j = 0; j < matr.GetLength(1); j++)
+
+
+            if (i > 0 && j > 0)
+                if (i % 2 == 0 && j % 2 == 0)
+
+                    Console.Write($" Четный индекс : {i},{j}");
+    Console.WriteLine();
+    Console.Write($" Элемент четного индекса : {matr[2, 2]}");
+    Console.WriteLine();
+
+
+}
+
+void ChangeElement(double[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+
+            if (i > 0 && j > 0)
+                if (i % 2 == 0 && j % 2 == 0)
+
+                    matr[2, 2] = Math.Pow(matr[2, 2], 2);
+
+        }
+
+
+    }
+}
+double[,] matrix = { { 2, 4, 3 }, { 4, 2, 1 }, { 1, 4, 4}, { 3, 2, 1} };
+Console.WriteLine("Массив : ");
+PrintArray(matrix);
+Console.WriteLine();
+FindEvenIndex(matrix);
+Console.WriteLine();
+ChangeElement(matrix);
+Console.WriteLine("Измененный: ");
+PrintArray(matrix);
