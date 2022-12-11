90b63996e31ef5fee32be0ed1e8e6e2fb6fe51b6
Темнова <nastena_petlilev@mail.ru>
2022-12-11 21:43:50 +0400

    lesson 7

---
+++ 71/Program.cs
@@ -0,0 +1,10 @@
+﻿// 71. Показать натуральные числа от N до 1, N задано
+void Loop(int N,int i = 1)
+{
+    if(i<=N)
+    {
+        System.Console.Write($"{N,5}");
+        Loop(N-1);
+    }
+}
+Loop(7); 
